ca5ee1d91b9e872178079c1e8addb7c422a2f2fb
MichaelWright4200 <michaelborn591@hotmail.com>
2016-05-10 21:34:22 -1000

    finsihed ex1

--- loops.js
+++ loops.js
@@ -1,9 +1,8 @@
-// Do your work here
-
+// Do your work here 
+/*  #1 */
 var prez = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
 
-function loopPrez(num) {
+
 	for(var i = 0; i<prez.length; i++) {
 console.log("The value of i is" + [i]);
 console.log("Value at index is" + prez[i]);}
-}
