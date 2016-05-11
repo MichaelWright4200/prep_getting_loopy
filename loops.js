3445fe61d303afa088d0396b38681a10a772731e
MichaelWright4200 <michaelborn591@hotmail.com>
2016-05-10 21:25:24 -1000

    complete prez exercise

--- loops.js
+++ loops.js
@@ -1,1 +1,9 @@
 // Do your work here
+
+var prez = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+
+function loopPrez(num) {
+	for(var i = 0; i<prez.length; i++) {
+console.log("The value of i is" + [i]);
+console.log("Value at index is" + prez[i]);}
+}
